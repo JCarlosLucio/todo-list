f9650c47d5345ae50639093c10651af56e578cd2
jcLucio <c.lucio3@gmail.com>
2020-05-24 16:47:18 -0500

    Add getDOMStrings

--- src/app/uiController.js
+++ src/app/uiController.js
@@ -1,4 +1,13 @@
 const uiController = (() => {
 	//  CODE FOR THE UI
+	const DOMStrings = {
+		addListBtn: '.add__list__btn'
+	};
+
+	const getDOMStrings = () => {
+		return DOMStrings;
+	};
+
+	return { getDOMStrings };
 })();
 export default uiController;

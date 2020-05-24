5cd873b5922f6f9f045080c0a7323676fbb889cc
jcLucio <c.lucio3@gmail.com>
2020-05-24 17:29:27 -0500

    Add getListInput

--- src/app/uiController.js
+++ src/app/uiController.js
@@ -1,13 +1,18 @@
 const uiController = (() => {
 	//  CODE FOR THE UI
 	const DOMStrings = {
-		addListBtn: '.add__list__btn'
+		addListBtn: '.add__list__btn',
+		addListTitle: '.add__list__title'
 	};
-
+	// ===== PUBLIC =====
+	const getListInput = () => {
+		const listTitle = document.querySelector(DOMStrings.addListTitle).value;
+		return listTitle;
+	};
 	const getDOMStrings = () => {
 		return DOMStrings;
 	};
 
-	return { getDOMStrings };
+	return { getDOMStrings, getListInput };
 })();
 export default uiController;

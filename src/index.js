12a914512cddcbfc718b8c84a7ea650d6cea6bed
jcLucio <c.lucio3@gmail.com>
2020-05-24 16:37:59 -0500

    Add setupEventListeners
    - Add ctrlAddListItem for test

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ import uiController from './app/uiController';
 
 const controller = ((listCtrl, uiCtrl) => {
 	// APP CONTROLLER
+
+	// Add item to list
+	const ctrlAddListItem = () => {
+		console.log('IM WORKING');
+		// 1. Get input data from lists field
+		// 2. Add new item to data-structure
+		// 3. Add new item to the lists ui
+	};
+
+	// Initilize Event Listeners
+	const setupEventListeners = () => {
+		const DOM = uiCtrl.getDOMStrings();
+		//  Add event handler for LISTS
+		document.querySelector(DOM.addListBtn).addEventListener('click', ctrlAddListItem);
+	};
+
+	return { setupEventListeners };
 })(listController, uiController);
 
 controller.setupEventListeners();

d262222a243984bababa46e7f4368d3cd5e0141c
jcLucio <c.lucio3@gmail.com>
2020-05-24 16:15:07 -0500

    Add controller
    -Import listController and uiController
    -Setup initial boilerplate for controller

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './main.scss';
+import listController from './app/listController';
+import uiController from './app/uiController';
+
+const controller = ((listCtrl, uiCtrl) => {
+	// APP CONTROLLER
+})(listController, uiController);
+
+controller.setupEventListeners();

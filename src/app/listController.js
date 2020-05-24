c0bc80caf36b916741b7b507b8a4c94c56675742
jcLucio <c.lucio3@gmail.com>
2020-05-24 18:18:31 -0500

    Add List class

--- src/app/listController.js
+++ src/app/listController.js
@@ -1,5 +1,12 @@
 const listController = (() => {
 	// CODE FOR THE DATA STRUCTURE
+	class List {
+		constructor(id, title) {
+			this.id = id;
+			this.title = title;
+			this.todos = [];
+		}
+	}
 })();
 
 export default listController;

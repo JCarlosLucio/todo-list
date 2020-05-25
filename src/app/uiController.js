ce19dbc9c74bf7f0c93cecd674dc2d3472859059
jcLucio <c.lucio3@gmail.com>
2020-05-24 19:17:09 -0500

    Add addListItem to uiController
    - Create list item elements
    - Append list item to list

--- src/app/uiController.js
+++ src/app/uiController.js
@@ -13,6 +13,26 @@ const uiController = (() => {
 		return DOMStrings;
 	};
 
-	return { getDOMStrings, getListInput };
+	const addListItem = function(obj) {
+		// create list item
+		const listItemDiv = document.createElement('div');
+		const titleDiv = document.createElement('div');
+		const trashIcon = document.createElement('i');
+		const deleteBtn = document.createElement('button');
+		listItemDiv.classList.add('item');
+		listItemDiv.setAttribute('id', `list-${obj.id}`);
+		titleDiv.classList.add('item__title');
+		titleDiv.innerText = obj.title;
+		trashIcon.classList.add('fas', 'fa-trash-alt');
+		deleteBtn.classList.add('item__delete__btn');
+		deleteBtn.append(trashIcon);
+		listItemDiv.append(titleDiv, deleteBtn);
+
+		// Append list item to lists
+		const list = document.querySelector('.list');
+		list.append(listItemDiv);
+	};
+
+	return { getDOMStrings, getListInput, addListItem };
 })();
 export default uiController;

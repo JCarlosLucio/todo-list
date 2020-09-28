29a81e7ce7120030e491be06baf5c47159b0e5d3
jcLucio <c.lucio3@gmail.com>
2020-09-28 18:20:06 -0500

    Update merge to {merge}

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,18 +1,18 @@
 const common = require('./webpack.common');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 
 module.exports = merge(common, {
-	mode: 'development',
-	module: {
-		rules: [
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					'style-loader', //3. Inject styles into DOM
-					'css-loader', //2. Turns css into commonjs
-					'sass-loader' //1. Turns sass into css
-				]
-			}
-		]
-	}
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader', //3. Inject styles into DOM
+          'css-loader', //2. Turns css into commonjs
+          'sass-loader', //1. Turns sass into css
+        ],
+      },
+    ],
+  },
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,31 +1,31 @@
 const path = require('path');
 const common = require('./webpack.common');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
 module.exports = merge(common, {
-	mode: 'production',
-	output: {
-		filename: 'main.[contentHash].js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	plugins: [
-		new MiniCssExtractPlugin({ filename: '[name].[contentHash].css' }),
-		new CleanWebpackPlugin(),
-		new MomentLocalesPlugin()
-	],
-	module: {
-		rules: [
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					MiniCssExtractPlugin.loader, //3. Extract css into files
-					'css-loader', //2. Turns css into commonjs
-					'sass-loader' //1. Turns sass into css
-				]
-			}
-		]
-	}
+  mode: 'production',
+  output: {
+    filename: 'main.[contentHash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '[name].[contentHash].css' }),
+    new CleanWebpackPlugin(),
+    new MomentLocalesPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader, //3. Extract css into files
+          'css-loader', //2. Turns css into commonjs
+          'sass-loader', //1. Turns sass into css
+        ],
+      },
+    ],
+  },
 });

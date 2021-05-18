0d9fde36c99f58339bee622e645243732ddb8fb1
jcLucio <c.lucio3@gmail.com>
2021-05-18 17:34:07 -0500

    Update contentHash to contenthash for cacheBusting

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,11 +8,11 @@ const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 module.exports = merge(common, {
   mode: 'production',
   output: {
-    filename: 'main.[contentHash].js',
+    filename: 'main.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: '[name].[contentHash].css' }),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
     new CleanWebpackPlugin(),
     new MomentLocalesPlugin(),
   ],
